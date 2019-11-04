c949c5ddd08a7c6071546d65c5408cb25aaffca5
Frankleiton Levy <frankleiton@gmail.com>
2019-11-04 14:03:41 -0300

    nada com nada

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,13 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
-import java.util.List;
-
-
-import org.opencv.core.*;
-import org.opencv.imgcodecs.Imgcodecs;
-import org.opencv.imgproc.Imgproc;
-import org.opencv.objdetect.HOGDescriptor;
 
 import org.opencv.core.Core;
 
@@ -25,23 +18,6 @@ public class Main extends Application {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 	}
 	
-	
-	
-	private void resizeImgame(String image_url) 
-	{
-		HOGDescriptor hog = new HOGDescriptor();
-		Mat img = new Mat();
-		MatOfFloat features = new MatOfFloat();
-		
-		img = Imgcodecs.imread(image_url, Imgcodecs.IMREAD_GRAYSCALE);
-		Imgproc.resize(img, img, new Size(64,128), 0.5, 0.5, Imgproc.INTER_LINEAR);
-		hog.compute(img,features);
-		List<Float> arrayOfFeatures = features.toList();
-		
-		for (Float float1 : arrayOfFeatures) {
-			System.out.println(float1);
-		}
-	}
 	
 	
 	@Override
--- src/br/imd/control/PrincipalController.java
+++ src/br/imd/control/PrincipalController.java
@@ -1,6 +1,14 @@
 package br.imd.control;
 
 import java.io.File;
+import java.util.List;
+
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfFloat;
+import org.opencv.core.Size;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.HOGDescriptor;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -20,13 +28,32 @@ public class PrincipalController {
 
 	public void ButtonSelectImage(ActionEvent event) {
 		FileChooser fc = new FileChooser();
-		fc.getExtensionFilters().addAll(new ExtensionFilter("Image Files", "*.jpg ", "*.png"));
+//		fc.getExtensionFilters().addAll(new ExtensionFilter("Image Files", "*.jpg ", "*.png"));
 		File selectedFile = fc.showOpenDialog(null);
 		
 		if (selectedFile != null) {
 			label_urlImage.setText("imagem Selecionada: "+selectedFile.getAbsolutePath());
+			resizeImgame(selectedFile.getAbsolutePath());
 		}else {
 			label_urlImage.setText("Imagem invalida");
+		}
+	}
+	
+	private void resizeImgame(String image_url) 
+	{
+		HOGDescriptor hog = new HOGDescriptor();
+		Mat img = new Mat();
+		MatOfFloat features = new MatOfFloat();
+		
+		img = Imgcodecs.imread(image_url, Imgcodecs.IMREAD_GRAYSCALE);
+		Imgproc.resize(img, img, new Size(64,128), 0.5, 0.5, Imgproc.INTER_LINEAR);
+		hog.compute(img,features);
+		List<Float> arrayOfFeatures = features.toList();
+		
+		System.out.println("Tamanho: "+ arrayOfFeatures.size());
+		
+		for (float float1 : arrayOfFeatures) {
+			System.out.println(float1);
 		}
 	}
 }
